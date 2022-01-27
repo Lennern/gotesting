58ad63934b86823e70995dce5046a1a511c933ba
Lukas <89383438+Lennern@users.noreply.github.com>
2022-01-28 00:59:56 +0100

    nice

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Lennern/gotesting/myquote"
 )
 
 func main() {
-	fmt.Println(del2pakke.myquote())
+	fmt.Println(myquote.GetGo())
+	fmt.Println(myquote.GetGlass())
+	fmt.Println(myquote.GetOpt())
+	fmt.Println(myquote.GetHello())
 }
